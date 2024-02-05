7368aaeb0c87cc25fda2eeb95fe5d2920c79ff1c
Mitali <mitaliactive@gmail.com>
2024-02-05 10:34:22 -0800

    add api_key and gitignore

--- main.py
+++ main.py
@@ -3,20 +3,19 @@ import pandas as pd
 import spacy
 import en_core_web_sm
 
+#api key: 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
+
 nlp = spacy.load("en_core_web_sm")
 nlp = en_core_web_sm.load()
 
+#open excel spreadsheet
 df = pd.read_excel("Research keywords - 2024.01.10.xlsx")
 
-def find_similarity(keywords1, keywords2): 
-    return 0;
-
-
+#convert keywords in each row into a list
 def make_keywords_list(df, row_index): 
     row_values = df.iloc[row_index].tolist()
     row_values = row_values[6].split("; ")
-    doc = nlp(row_values)
-    keywords_object = set(token.text.lower() for token in docz if not token.is_stop and token.is_alpha)
+    return row_values
 
 # Display the values
 ##print(f"Values in row {row_index + 1}: {row_values}")
@@ -24,7 +23,8 @@ num_rows = df.shape[0]
 #print(num_rows)
 #print(make_keywords_list(df, 2))
 keywords1 = make_keywords_list(df, 6)
-keywords2 = make_keywords_list(df, 31)
-print(len(keywords1 | keywords2))
-print(len(keywords1 & keywords2))
+keywords2 = make_keywords_list(df, 25)
+print(keywords1)
+print()
+print(keywords2)
 

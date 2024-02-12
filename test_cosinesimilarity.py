69e9baf1c78a3b1f0034d735ac6767ab61c800e7
Mitali <mitaliactive@gmail.com>
2024-02-12 13:33:02 -0800

    Update test_cosinesimilarity.py
    imported model

--- test_cosinesimilarity.py
+++ test_cosinesimilarity.py
@@ -1,14 +1,32 @@
 import pandas as pd
+import gensim
 from gensim.models import Word2Vec
-from collections import defaultdict
-import spacy
-import logging
+from gensim.models import KeyedVectors
+from gensim.test.utils import datapath
+import numpy as np
+from numpy.linalg import norm
 
+wv_from_bin = KeyedVectors.load_word2vec_format(datapath(
+    '/Users/mitalimittal/keyword_matching/GoogleNews-vectors-negative300.bin'), binary=True)
+
+#result = wv_from_bin.doesnt_match("breakfast cereal puppy lunch".split())
+vector1 = wv_from_bin['Alzheimers']
+
+
+#cosine for Alzheimers and Dementia: 0.6454646
+#cosine for Alzheimers and Dog: 0.17926827
+#cosine for Alzheimers and Memory: 0.31218246
+#cosine for Alzheimers and Alzheimers: 1.0
+
+
+print(vector1)
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print(vector2)
+
+cosine = np.dot(vector1, vector2)/ (norm(vector1)*norm(vector2))
+print(cosine)
 
 df = pd.read_excel("Research keywords - 2024.01.10.xlsx") #open excel spreadsheet
-
-model_name = "BioWordVec_PubMed_MIMICIII_d200.vec.bin"
-model = Word2Vec.load("/Users/mitalimittal/Downloads/BioWordVec_PubMed_MIMICIII_d200.vec.bin")
 
 #convert keywords in each row into a list
 def make_keywords_list(df, row_index): 
@@ -20,12 +38,14 @@ def make_keywords_list(df, row_index):
 #key: the professor's full name
 #value: the keywords associated with that professor
 professors = {}
+keyword_arr = []
 
 #make professors dictionary
 for i in range(df.shape[0]): 
     keywords = make_keywords_list(df, i)
     professor_name = df.iloc[i].tolist()[0]
     professors[professor_name] = keywords
+    keyword_arr.append(keywords)
 
 prof1 = 'Yassa, Michael'
 keywords1 = professors[prof1]
